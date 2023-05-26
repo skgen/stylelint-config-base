9b7d6dbc62c9b0c6171adfca40d0a42c2f254e91
Lucas Chapuis <l.chapuis@patriarche.fr>
2023-05-26 18:39:01 +0200

    feat: exporting specific scss rules to global ones

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ module.exports = {
     'stylelint-config-standard-scss',
     'stylelint-config-recess-order',
   ],
-  overrides: [
-    {
-      files: ['*.scss', '**/*.scss'],
-      rules: {
-        'unit-allowed-list': ['px', '%', 'vh', 'vw', 'deg', 'fr', 'ms', 'em'],
-        'value-no-vendor-prefix': [true, { ignoreValues: ['box'] }],
-        'alpha-value-notation': 'number',
-        'max-line-length': 150,
-        'selector-class-pattern': [/[_\-a-zA-Z0-9]+/, { resolveNestedSelectors: true }],
-      },
-    },
-  ],
   rules: {
     indentation: [4, { baseIndentLevel: 0 }],
+    'unit-allowed-list': ['px', '%', 'vh', 'vw', 'deg', 'fr', 'ms', 'em'],
+    'value-no-vendor-prefix': [true, { ignoreValues: ['box'] }],
+    'alpha-value-notation': 'number',
+    'max-line-length': 150,
+    'selector-class-pattern': [/[_\-a-zA-Z0-9]+/, { resolveNestedSelectors: true }],
   },
   ignoreFiles: [
     '**/dist/**/*.scss',
